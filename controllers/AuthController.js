f60feda88d4766a24cead7a4444d586c725079b2
*Gustavo <gustavobbacco@gmail.com>
2025-01-10 18:20:17 -0300

    feat: implementing flash messages

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,26 @@
-module.exports = class AuthController {
-  static login(req, res) {
-    res.render("auth/login");
-  }
-  static register(req, res) {
-    res.render("auth/register");
-  }
-};
+  const User = require("../models/User");
+
+  const bcrypt = require("bcryptjs");
+
+  module.exports = class AuthController {
+    static login(req, res) {
+      res.render("auth/login");
+    }
+    static register(req, res) {
+      res.render("auth/register");
+    }
+
+    static async registerPost(req, res) {
+
+      const {name, email, password, confirmpassword} = req.body
+
+      // password match validation
+      if(password != confirmpassword ){
+        req.flash('message', 'As senhas não conferem, tente novamente!')
+        res.render('auth/register')
+
+        return
+      }
+
+    }
+  };
